375e80e7faa30ffdb8b4564cfb129d9a04ee8ea7
aligilljutt150 <aligilljutt150@gmail.com>
2026-03-23 18:02:42 +0000

    update the system files

--- _worker.js
+++ _worker.js
@@ -1,10 +1,18 @@
 
 import { Router } from 'worktop';
-import { listen } from 'worktop/cache';
-import api from './functions/api';
 
 const router = new Router();
 
-router.add('*', '/', api.fetch);
+router.add('*', '/', async (req, res) => {
+  try {
+    const { default: api } = await import('./functions/api.js');
+    return await api.fetch(req, res);
+  } catch (error) {
+    console.error(error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
+});
 
-listen(router.run);
+addEventListener('fetch', (event) => {
+  event.respondWith(router.run(event));
+});
